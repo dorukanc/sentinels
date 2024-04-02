06a96399415faf1ce4a8a33e8a22af94ee7440b7
dorukanc <catakd@mef.edu.tr>
2024-04-02 22:10:51 +0300

    fitering and plotting the pt_dt data

--- data_analysis.py
+++ data_analysis.py
@@ -9,18 +9,28 @@ df = pd.read_csv('data/covid19_mortalityrate_weekly_oecd.csv')
 # Checking the first few rows to ensure our data loaded correctly
 print(df.head())
 
-# filtering the data
+# filtering the data, pt_dt (percentage of death total)
 
-# extracting the data from OBS_VALUE column
-obs_value = df['OBS_VALUE']
+filtered_pt_data = df[df['UNIT_MEASURE'] == 'PT_DT']
 
-
+# extracting data from filtered data
+obs_value = filtered_pt_data['OBS_VALUE']
 
 # mean, median, std of our data
 
 mean_value = obs_value.mean()
 median_value = obs_value.median()
 std_value = obs_value.std()
+
+# Plotting the mortality rate to a graph weekly
+# Created a line plot to visualize mortality rate (pt_dt)
+plt.figure(figsize=(8,6))
+plt.plot(filtered_pt_data['OBS_VALUE'], marker='o', linestyle='-', color='b')
+plt.xlabel("Week")
+plt.ylabel("Mortality Rate")
+plt.title("Weekly Covid-19 Mortality Rate")
+plt.grid()
+plt.show()
 
 # Print the results
 print(f"Mean: {mean_value:.2f}")
